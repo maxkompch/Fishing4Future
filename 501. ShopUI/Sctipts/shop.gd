c99d63001cb06ff0fd82d7ee50a83b7e58c49089
imon333 <imonhosen99@gmail.com>
2024-11-17 20:14:15 +0100

    New folder structure has changed with (501. shopUI) and will combined with mehmood additional buttion system for purchase . So later on from mehmood will be used cause that would be the updated one

--- 501. ShopUI/Sctipts/shop.gd
+++ 501. ShopUI/Sctipts/shop.gd
@@ -13,4 +13,4 @@ func _process(delta: float) -> void:
 
 func _on_scene_transition_timer_timeout() -> void:
 	print("Timer triggered")  # This should print when the timer reaches 2 seconds
-	get_tree().change_scene_to_file("res://6. TextScrolls/Scenes/inside_shop.tscn")
+	get_tree().change_scene_to_file("res://501. ShopUI/Scenes/inside_shop.tscn")
