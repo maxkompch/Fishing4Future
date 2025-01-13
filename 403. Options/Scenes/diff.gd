beeb9fbace9e0431c643e1fabab92e7aa316d366
Mehmood Ali <mehmoodshaikh011@gmail.com>
2025-01-13 19:21:06 +0100

    cut to intro scene before court

--- 403. Options/Scenes/diff.gd
+++ 403. Options/Scenes/diff.gd
@@ -9,18 +9,18 @@ func _on_easy_pressed() -> void:
 	GameData.difficulty_index = 0
 	GameData.save_data()
 	time_system.log("easy")
-	get_tree().change_scene_to_file("res://401. Cut Scene/Scenes/court_scene1.tscn")
+	get_tree().change_scene_to_file("res://416. Opening Scenes/Scenes/intro.tscn")
 
 
 func _on_normal_pressed() -> void:
 	GameData.difficulty_index = 1
 	GameData.save_data()
 	time_system.log("normal")
-	get_tree().change_scene_to_file("res://401. Cut Scene/Scenes/court_scene1.tscn")
+	get_tree().change_scene_to_file("res://416. Opening Scenes/Scenes/intro.tscn")
 
 
 func _on_hard_pressed() -> void:
 	GameData.difficulty_index = 2
 	GameData.save_data()
 	time_system.log("hard")
-	get_tree().change_scene_to_file("res://401. Cut Scene/Scenes/court_scene1.tscn")
+	get_tree().change_scene_to_file("res://416. Opening Scenes/Scenes/intro.tscn")
